6b2587e67d22a40b0b7460f3f7e526e78c944cae
Christoforos-Marios Mamaloukas <xmamalou@cyber-mantid.xyz>
2025-05-31 00:10:59 +0300

    Added LICENSE

--- src/main.cxx
+++ src/main.cxx
@@ -1,3 +1,22 @@
+/*
+    Parallel Systems Exercise Batch 2 -- Solutions to Batch 2 of Exercises for the Parallel
+    Systems Course of the "Computer Engineering" Masters Programme of NKUA
+    Copyright (C) 2025 Christoforos-Marios Mamaloukas
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include <iostream>
 #include <string_view>
 #include <vector>
